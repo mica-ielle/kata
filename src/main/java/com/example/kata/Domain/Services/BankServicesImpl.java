d7c5bbf339d9645892aea6538641e169d8f028b9
Micaelle Nzoueteng <nzouetengmiaelle@gmail.com>
2025-01-30 23:40:35 +0100

    implementation de consulter les transaction precedentes

--- src/main/java/com/example/kata/Domain/Services/BankServicesImpl.java
+++ src/main/java/com/example/kata/Domain/Services/BankServicesImpl.java
@@ -1,6 +1,7 @@
 package com.example.kata.Domain.Services;
 
 import com.example.kata.Domain.model.Debit;
+import com.example.kata.Domain.model.Transaction;
 import com.example.kata.Port.dto.CreditRequestDTO;
 import com.example.kata.Domain.model.Amount;
 import com.example.kata.Domain.model.Credit;
@@ -9,20 +10,26 @@ import com.example.kata.Port.input.BankServices;
 import com.example.kata.Port.output.AmountRepository;
 import com.example.kata.Port.output.CreditRepository;
 import com.example.kata.Port.output.DebitRepository;
+import com.example.kata.Port.output.TransactionRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class BankServicesImpl implements BankServices {
 
     private CreditRepository creditRepository;
     private DebitRepository debitRepository;
-
+    private TransactionRepository transactionRepository;
     private AmountRepository amountRepository;
 
 
-    public BankServicesImpl(CreditRepository creditRepository, DebitRepository debitRepository, AmountRepository amountRepository) {
+    public BankServicesImpl(CreditRepository creditRepository, DebitRepository debitRepository, TransactionRepository transactionRepository, AmountRepository amountRepository) {
         this.creditRepository = creditRepository;
         this.debitRepository = debitRepository;
+        this.transactionRepository = transactionRepository;
         this.amountRepository = amountRepository;
     }
 
@@ -32,6 +39,7 @@ public class BankServicesImpl implements BankServices {
 
         //registrate credit transaction
         creditRepository.add(credit);
+        transactionRepository.add(credit);
         //increase curent amount
         double amount = amountRepository.currentAmount().getAmount();
         double na = amount+ credit.getAmount();
@@ -49,6 +57,7 @@ public class BankServicesImpl implements BankServices {
 
         //registrate debit transaction
         debitRepository.add(debit);
+        transactionRepository.add(debit);
         //decrease curent amount
         double amount = amountRepository.currentAmount().getAmount();
 
@@ -74,5 +83,12 @@ public class BankServicesImpl implements BankServices {
     @Override
     public Amount curentAmount() {
         return amountRepository.currentAmount();
+    }
+
+    @Override
+    public List<Transaction> previousTransactions(int index) {
+        List<Transaction> transactionList = transactionRepository.findAll();
+
+        return transactionList.subList(0,index);
     }
 }
--- src/main/java/com/example/kata/Port/input/BankServices.java
+++ src/main/java/com/example/kata/Port/input/BankServices.java
@@ -3,8 +3,11 @@ package com.example.kata.Port.input;
 import com.example.kata.Domain.model.Amount;
 import com.example.kata.Domain.model.Credit;
 import com.example.kata.Domain.model.Debit;
+import com.example.kata.Domain.model.Transaction;
 import com.example.kata.Port.dto.CreditRequestDTO;
 import com.example.kata.Port.dto.DebitRequestDTO;
+
+import java.util.List;
 
 public interface BankServices {
 
@@ -13,4 +16,6 @@ public interface BankServices {
     DebitRequestDTO debit(Debit debit);
 
     Amount curentAmount();
+
+    List<Transaction> previousTransactions(int index);
 }
